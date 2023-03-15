27260441f1dd8de47638625b848997b1557408d5
ACM-Anik <anikmojumder5252@gmail.com>
2023-03-15 22:20:33 +0600

    submit button's bug of addEventListener corrected

--- index.js
+++ index.js
@@ -57,9 +57,8 @@ const displayQuiz = (data) => {
     quizContainer.innerHTML = "";
     return;
   }
-  data.forEach((quiz, i) => {
-  // data.forEach( quiz => {//(quiz, i)  
-    console.log(quiz);
+  data.forEach((quiz, i) => {  
+    // console.log(quiz);
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
@@ -75,7 +74,7 @@ const displayQuiz = (data) => {
 };
 
 // EventListener for quiz submit button
-document.querySelector("#submit").addEventlistener("click", () => {
+document.querySelector("#submit").addEventListener("click", () => {
   if (answers.length < 6) {
     return;
   }
